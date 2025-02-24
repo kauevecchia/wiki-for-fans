367898e28c60d6aedb14f2b72a91df89439426e5
Kauê <kauevecchia@hotmail.com>
2025-02-24 20:18:46 -0300

    style: import Libre Baskerville and Poppins fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+                baskerville: ['Libre Baskerville', 'serif'],
+            }
+        },
     },
     plugins: [],
 }
