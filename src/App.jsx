f7b6dc84ba9816c0218349957b39fc85d6d8ee6b
Kauê <kauevecchia@hotmail.com>
2025-02-25 01:49:06 -0300

    feat: add fetch request to get characters data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { Header } from "./components/Header"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 export function App() {
   const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    fetch('https://stranger-things-api.fly.dev/api/v1/characters/?perPage=108')
+      .then((res) => res.json())
+      .then(data => setCharacters(data))
+      .catch((error) => {
+        console.error(`Failed to fetch character data: ${error}`)
+      })
+  }, [])
 
   return (
     <div className="font-poppins">
