c67c62ba0e3460cd30e64dbce2df5709aadf31a5
Kauê <kauevecchia@hotmail.com>
2025-02-25 01:48:11 -0300

    feat: add state to store the api response

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Header } from "./components/Header"
+import { useState } from "react"
 
 
 export function App() {
-  
+  const [characters, setCharacters] = useState([])
+
   return (
     <div className="font-poppins">
       <Header />
