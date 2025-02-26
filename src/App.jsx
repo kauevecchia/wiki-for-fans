bd722781caa2ea50aff721804e1c04a99e39f75a
Kauê <kauevecchia@hotmail.com>
2025-02-26 10:24:38 -0300

    feat: add loading spinner while fetching character data

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,15 @@ export function App() {
   const [characters, setCharacters] = useState([])
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [selectedCharacter, setSelectedCharacter] = useState({})
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch('https://stranger-things-api.fly.dev/api/v1/characters/?perPage=108')
       .then((res) => res.json())
-      .then(data => setCharacters(data))
+      .then(data => {
+        setLoading(false)
+        setCharacters(data)
+      })
       .catch((error) => {
         console.error(`Failed to fetch character data: ${error}`)
       })
@@ -29,31 +33,38 @@ export function App() {
   }
 
   return (
-    <div className="font-poppins">
+    <div className="font-poppins flex flex-col min-h-screen">
       <Header />
 
-      <main className="flex flex-col items-center justify-center gap-10 bg-zinc-900 px-8 py-16">
+      <main className="flex flex-col items-center justify-center gap-10 bg-zinc-900 px-8 py-16 flex-grow">
 
-        <div className="max-w-7xl grid md:grid-cols-3 lg:grid-cols-4 gap-12">
-          {characters.map(character => {
-            if (!character.name || !character.name || !character.gender || !character.portrayedBy) {
-              return null
-            }
-            return <Card
-              key={character._id}
-              toggleModal={toggleModal}
-              characterInfo={{
-                name: character.name,
-                status: character.status,
-                born: character.born,
-                gender: character.gender,
-                portrayedBy: character.portrayedBy,
-                imgUrl: character.photo,
-                aliases: character.aliases,
-              }}
-            />
-          })}
-        </div>
+        {loading ? (
+          <div className="h-screen flex justify-center items-center gap-4">
+            <p className="text-white">Loading</p>
+            <div className="w-12 h-12 border-t-4 border-red-600 border-solid rounded-full animate-spin"></div>
+          </div>
+        ) : (
+          <div className="max-w-7xl grid md:grid-cols-3 lg:grid-cols-4 gap-12">
+            {characters.map(character => {
+              if (!character.name || !character.name || !character.gender || !character.portrayedBy) {
+                return null
+              }
+              return <Card
+                key={character._id}
+                toggleModal={toggleModal}
+                characterInfo={{
+                  name: character.name,
+                  status: character.status,
+                  born: character.born,
+                  gender: character.gender,
+                  portrayedBy: character.portrayedBy,
+                  imgUrl: character.photo,
+                  aliases: character.aliases,
+                }}
+              />
+            })}
+          </div>
+        )}
       </main>
 
       {isModalOpen && <Modal
