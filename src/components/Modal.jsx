80872197c82d0b70f114fb53174382682d4d3859
Kauê <kauevecchia@hotmail.com>
2025-02-26 17:06:32 -0300

    chore: adjust modal animation duration from 0.3s to 0.5s

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,7 +10,7 @@ export function Modal({ selectedCharacter, toggleModal }) {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
+            transition={{ duration: 0.5 }}
         >
             <div className="bg-zinc-900 p-4 md:p-8 rounded-md flex items-center justify-center flex-col max-w-xl">
                 <div>
