249d22ef72f57d294b27a2b3da2682b785274b28
Kauê <kauevecchia@hotmail.com>
2025-02-26 01:28:34 -0300

    style: adjust header layout and responsiveness for larger screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ export function Header() {
     return (
         <header className="min-h-24 font-baskerville flex items-center justify-start bg-black text-red-600 p-8 border-b-2 border-red-700">
             <div className="max-w-7xl w-full mx-auto flex items-center justify-between">
-                <h1 className="text-lg md:text-2xl font-bold">Wiki For Fans</h1>
+                <h1 className="text-lg md:text-2xl lg:text-3xl font-bold">Wiki For Fans</h1>
                 
 
-                <div className='w-28 h-auto md:w-48 md:h-auto'>
+                <div className='w-28 h-auto md:w-48 md:h-auto lg:w-56'>
                     <img src={stLogo} alt="Stranger Things logo" className="w-full h-full object-cover"/>
                 </div>
             </div>
