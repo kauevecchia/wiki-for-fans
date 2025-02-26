458b6dd0c77e44c69eb3c1d1aaf23e2540b7c1ff
Kauê <kauevecchia@hotmail.com>
2025-02-25 21:07:13 -0300

    feat: add logo and wrap content with max-width container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
+import stLogo from '../assets/st-logo.png'
+
 export function Header() {
     return (
-        <header className="min-h-24 font-baskerville flex items-center justify-start bg-black text-red-600 p-8">
-            <div>
-                <h1 className="text-xl font-bold">Wiki For Fans</h1>
+        <header className="min-h-24 font-baskerville flex items-center justify-start bg-black text-red-600 p-8 border-b-2 border-red-700">
+            <div className="max-w-7xl w-full mx-auto flex items-center justify-between">
+                <h1 className="text-lg md:text-2xl font-bold">Wiki For Fans</h1>
+                
+
+                <div className='w-28 h-auto md:w-48 md:h-auto'>
+                    <img src={stLogo} alt="Stranger Things logo" className="w-full h-full object-cover"/>
+                </div>
             </div>
         </header>
     )
